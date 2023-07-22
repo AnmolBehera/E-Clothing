839ca0d2e2b49656d4e5e23da7e66bca44657997
BISHAL PRADHAN <bishalpradhan061@gmail.com>
2023-07-22 10:58:40 +0530

    footer commited

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
+    <ChakraProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </ChakraProvider>
     </MantineProvider>
   </React.StrictMode>
 );
