4998c4a4557d69e5627674ced74e380733fe2d46
BISHAL PRADHAN <bishalpradhan061@gmail.com>
2023-07-27 12:00:06 +0530

    Allroutes.jsx file updated

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/signup/signup";
 import homepage from "./pages/homepage";
 import SignIn from "./pages/signin";
-
+import About from "./pages/aboutus";
 export default function AllRoutes() {
     return (
         <Routes>
             <Route path="/signup" element={<SignUp />} />
             <Route path="/" element={<homepage/>}/>
             <Route path="/signin" element={<SignIn />} />
+            <Route path="/aboutus" element={<About />} />
         </Routes>
     )
 }
